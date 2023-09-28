635e712b7abeb5b1a2378b385e30bc9ceae2b8eb
MUHAMMAD UMER MAHMOOD <55051631+Umer-prog@users.noreply.github.com>
2023-09-28 13:57:50 +0500

    Postfix to infix

---
+++ Data Structures/Expressions/Postfixtoinfix.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string PrefixToInfix(string w) {
+    stack<string> stack;
+    string first;
+    string second;
+    char ch;
+    string ch1;
+    string infix = "";
+    int size = w.length();
+    for (int i = 1; i <= size; i++)
+    {
+        ch = w[size - i];
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            ch1 = ch;
+            stack.push(ch1);
+        }
+        
+        else
+        {
+            first = stack.top();
+            stack.pop();
+            second = stack.top();
+            stack.pop();
+            infix = "(" + first + ch + second + ")";
+
+            stack.push(infix);
+        }
+        
+    }
+    return infix;
+}
+
+int main() {
+    string exp = "*+AB/EF";
+    cout << "Infix is: " << PrefixToInfix(exp);
+}
